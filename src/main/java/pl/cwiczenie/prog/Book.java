6f7a9ddc97bab3d9c65fe40cb896609d86134ab3
Luks <luki352@op.pl>
2026-01-02 22:13:08 +0100

    Dodanie konstruktora w klasie Book i zmniejszenie powtarzającego sie kodu w klasie Libray

--- src/main/java/pl/cwiczenie/prog/Book.java
+++ src/main/java/pl/cwiczenie/prog/Book.java
@@ -7,4 +7,18 @@ public class Book {
     int pages;
     String publisher;
     String isbn;
+    Book(String bookTitle,String bookauthor, int bookReleaseDate,
+         int bookPages, String  bookPublisher, String bookIsbn){
+        title = bookTitle;
+        author = bookauthor;
+        releaseDate = bookReleaseDate;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+    void printInfo(){
+        String info = "Tytuł: "+ title + "\n"+" Autor: " + author + "\n Rok wydania: " + releaseDate+
+                "\n Ilość stron: "+ pages + "\n wydawnictwo: "+ publisher + "\n Numer ISBN: "+ isbn;
+        System.out.println(info);
+    }
 }
