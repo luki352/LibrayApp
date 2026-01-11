f42dcb5f79f10dbee706e0923605e25877606ff5
Luks <luki352@op.pl>
2026-01-11 19:22:02 +0100

    Stworzenie tablicy, która przechowuje ksiązki dostepne w bibliotece

--- src/main/java/pl/developzpl/libray/app/Libray.java
+++ src/main/java/pl/developzpl/libray/app/Libray.java
@@ -6,23 +6,24 @@ public class Libray {
     public static void main(String[] args) {
         final String appName = "Biblioteka v0.4";
 
-        Book book1 = new Book("W pustyni i puszczy","Henryk Sienkiewicz", 2010,
-                296, "Greg", "9788373271890");
-        Book book2 = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz",2023, 358,
+        Book [] books  = new Book[1000];
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296,
+                "Greg", "9788373271890");
+
+        books[1] = new Book("Ogniem i Mieczem", "Henryk Sienkiewicz",2023, 358,
                 "PWN", "9784273271875");
-        Book book3 = new Book("Bajki dla dzieci", "Jan Brzechwa", 1998, 45,
+        books[2] = new Book("Bajki dla dzieci", "Jan Brzechwa", 1998, 45,
                 "WAM");
 
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibiliotece:");
 
-        book1.setPages(150);
-        System.out.println("Książka pierwsza w katalogu:  "+ book1.getPages());
 
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
+        System.out.println("System może przechowywać do "+ books.length +" książek");
 
 
     }
